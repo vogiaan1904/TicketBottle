2a1aab31a6fef2f310d3d8496f96d834faeff296
Viet Hung <viethung01234@gmail.com>
2024-12-16 08:58:41 +0700

    config: dockerfile

--- src/modules/database/database.service.ts
+++ src/modules/database/database.service.ts
@@ -4,7 +4,6 @@ import {
   Logger,
   OnModuleInit,
 } from '@nestjs/common';
-import * as bcrypt from 'bcryptjs';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
@@ -17,34 +16,33 @@ export class DatabaseService extends PrismaClient implements OnModuleInit {
     try {
       await this.$connect();
       this.appLogger.log('Connected to database successfully');
-      await this.createAdminAccount();
     } catch (error) {
       this.appLogger.error('Failed to connect to database', error);
       throw new InternalServerErrorException(error);
     }
   }
 
-  async createAdminAccount() {
-    const existedAdmin = await this.staff.findFirst({
-      where: {
-        username: 'admin',
-      },
-    });
-    if (existedAdmin) {
-      this.appLogger.log('Admin account is found');
-      return;
-    }
-    this.appLogger.error(
-      'Admin account is not found. Creating admin account...',
-    );
-    const hashedPassword = await bcrypt.hash('admin123', 10);
-    await this.staff.create({
-      data: {
-        username: 'admin',
-        password: hashedPassword,
-        role: 'ADMIN',
-      },
-    });
-    this.appLogger.log('Create admin account successfully');
-  }
+  //   async createAdminAccount() {
+  //     const existedAdmin = await this.staff.findFirst({
+  //       where: {
+  //         username: 'admin',
+  //       },
+  //     });
+  //     if (existedAdmin) {
+  //       this.appLogger.log('Admin account is found');
+  //       return;
+  //     }
+  //     this.appLogger.error(
+  //       'Admin account is not found. Creating admin account...',
+  //     );
+  //     const hashedPassword = await bcrypt.hash('admin123', 10);
+  //     await this.staff.create({
+  //       data: {
+  //         username: 'admin',
+  //         password: hashedPassword,
+  //         role: 'ADMIN',
+  //       },
+  //     });
+  //     this.appLogger.log('Create admin account successfully');
+  //   }
 }
